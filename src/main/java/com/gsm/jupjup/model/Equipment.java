a98ed667309fd8eff45b4795040617d3ed4a0505
siwony_ <siwon103305@gmail.com>
2021-01-25 12:47:38 +0900

    [ADD]Equipmet.java equipmet domain finish

--- src/main/java/com/gsm/jupjup/model/Equipment.java
+++ src/main/java/com/gsm/jupjup/model/Equipment.java
@@ -1,2 +1,30 @@
-package com.gsm.jupjup.model;public class Equipment {
+package com.gsm.jupjup.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Equipment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long eq_Idx;
+
+    private String name;
+
+    @Lob
+    private byte[] img_equipment; //BLOB
+
+    private String content;
+
+    private int count;
+
+    public void updateCount(int Count){
+        this.count = Count;
+    }
 }
