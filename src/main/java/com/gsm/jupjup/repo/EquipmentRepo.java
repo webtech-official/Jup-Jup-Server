628b9fb8bb26a3d359c833e1331471b80095be58
siwony_ <siwon103305@gmail.com>
2021-01-25 14:55:09 +0900

    [ADD]equipmentRepo

--- src/main/java/com/gsm/jupjup/repo/EquipmentRepo.java
+++ src/main/java/com/gsm/jupjup/repo/EquipmentRepo.java
@@ -1,2 +1,10 @@
-package com.gsm.jupjup.repo;public interface EquipmentRepo {
+package com.gsm.jupjup.repo;
+
+import com.gsm.jupjup.model.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EquipmentRepo extends JpaRepository<Equipment, Long> {
+    Optional<Equipment> findByName(String name);
 }
