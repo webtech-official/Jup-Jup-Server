c475cdac02d8bcab7fcb04f29ac2eba68e098421
siwony_ <siwon103305@gmail.com>
2021-01-25 22:06:50 +0900

    [UPDATE]equipmentAllow update logic 아직 의사결정이 안되서 미완성으로 끝냄

--- src/main/java/com/gsm/jupjup/controller/v1/EquipmentAllowController.java
+++ src/main/java/com/gsm/jupjup/controller/v1/EquipmentAllowController.java
@@ -5,6 +5,8 @@ import com.gsm.jupjup.model.response.CommonResult;
 import com.gsm.jupjup.model.response.ResponseService;
 import com.gsm.jupjup.service.equipment.EquipmentAllowService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,9 +18,21 @@ public class EquipmentAllowController {
     private final EquipmentAllowService equipmentAllowService;
     private final ResponseService responseService; // 결과를 처리할 Service
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
     @PostMapping("/equipmentallow/{name}")
-    public CommonResult saveEquipmentAllowData(@PathVariable() String name, @RequestBody EquipmentAllowSaveDto equipmentAllowSaveDto) throws Exception {
+    public CommonResult save(@PathVariable() String name, @RequestBody EquipmentAllowSaveDto equipmentAllowSaveDto) throws Exception {
         equipmentAllowService.save(name, equipmentAllowSaveDto);
+        return responseService.getSuccessResult();
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @PutMapping("/equipmentallow/{eqa_idx}")
+    public CommonResult update(@PathVariable() Long eqa_idx, @RequestBody EquipmentAllowSaveDto equipmentAllowSaveDto){
+        equipmentAllowService.update(eqa_idx, equipmentAllowSaveDto);
         return responseService.getSuccessResult();
     }
 
--- src/main/java/com/gsm/jupjup/service/equipment/EquipmentAllowService.java
+++ src/main/java/com/gsm/jupjup/service/equipment/EquipmentAllowService.java
@@ -18,17 +18,46 @@ public class EquipmentAllowService {
     public final EquipmentService equipmentService;
 
     @Transactional
-    public void save(String NameOfEquipment , EquipmentAllowSaveDto equipmentAllowSaveDto) throws Exception {
+    public void save(String NameOfEquipment, EquipmentAllowSaveDto equipmentAllowSaveDto) throws Exception {
         //기자제 이름으로 equipment 테이블을 조회
         Equipment equipment = equipmentService.equipmentFindBy(NameOfEquipment);
         //equipment 조회해서 dto에 넣어줌
         equipmentAllowSaveDto.setEquipment(equipment);
         EquipmentAllow equipmentAllow = equipmentAllowSaveDto.toEntity();
         //기자제 남은 수량 계산
-        int equipmentAmount = equipment.getCount() - equipmentAllow.getAmount();
+        //int equipmentAmount = equipment.getCount() - equipmentAllow.getAmount();
 
         equipmentAllowRepo.save(equipmentAllow);
         //기자재 수량 업데이트
-        equipment.updateAmount(equipmentAmount);
+        //equipment.updateAmount(equipmentAmount);
     }
+
+    @Transactional
+    public void update(Long eqa_idx, EquipmentAllowSaveDto equipmentAllowSaveDto){
+        EquipmentAllow equipmentAllow = equipmentAllowFindBy(eqa_idx);
+        int equipmentCount = equipmentAllow.getAmount();
+        int equipmentAllowAmount = equipmentAllow.getAmount();
+
+        if(equipmentAmountCountChk(equipmentCount, equipmentAllowAmount))
+            equipmentAllow.update(equipmentAllow.getAmount(), equipmentAllow.getReason());
+        else
+            throw new IllegalArgumentException("현제 신청할 수 있는 기자제를 초과 했습니다.")
+
+
+        //equipmentAllow.update(newEquipmentAllowAmount, equipmentAllowSaveDto.getReason(), UpdateEquipmentCount);
+    }
+
+    public EquipmentAllow equipmentAllowFindBy(Long idx){
+        return equipmentAllowRepo.findById(idx).orElseThrow(IllegalAccessError::new);
+    }
+
+    //equipmentAllow
+    public boolean equipmentAmountCountChk(int equipmentCount, int newEquipmentAllowAmount){
+        if(equipmentCount >= newEquipmentAllowAmount)
+            return true;
+        else
+            return false;
+    }
+
+
 }
