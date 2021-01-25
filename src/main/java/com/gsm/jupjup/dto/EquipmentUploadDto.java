feb915f588765b832e46fd97450f50f66ba37cd7
siwony_ <siwon103305@gmail.com>
2021-01-25 12:46:56 +0900

    [ADD]equipmentUploadDto 기자제 업로드 및 byte 코드 변환 DTO

--- src/main/java/com/gsm/jupjup/dto/EquipmentUploadDto.java
+++ src/main/java/com/gsm/jupjup/dto/EquipmentUploadDto.java
@@ -1,2 +1,41 @@
-package com.gsm.jupjup.dto;public class EquipmentUploadDto {
+package com.gsm.jupjup.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gsm.jupjup.model.Equipment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EquipmentUploadDto {
+    private MultipartFile img_equipment;
+    private byte[] img_equipmentToByte;
+
+    private String name;
+    private String content;
+    private int count;
+
+    @Builder
+    public EquipmentUploadDto(MultipartFile img_equipment, String name, String content, int count) throws IOException {
+        this.img_equipment = img_equipment;
+        this.img_equipmentToByte = img_equipment.getBytes();
+        this.name = name;
+        this.content = content;
+        this.count = count;
+    }
+
+    public Equipment toEntity(){
+        return Equipment.builder()
+                .name(this.name)
+                .img_equipment(this.img_equipmentToByte)
+                .content(this.content)
+                .count(this.count)
+                .build();
+    }
 }
