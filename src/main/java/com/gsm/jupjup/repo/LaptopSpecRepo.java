9f783174cf66fea77175cd99f5d03425da66e2db
johnjihwan <s20062@gsm.hs.kr>
2021-01-26 13:46:26 +0900

    [ADD]LaptopSpecRepo

---
+++ src/main/java/com/gsm/jupjup/repo/LaptopSpecRepo.java
@@ -0,0 +1,8 @@
+package com.gsm.jupjup.repo;
+
+import com.gsm.jupjup.model.LaptopSpec;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LaptopSpecRepo extends JpaRepository<LaptopSpec, Long> {
+
+}
