ac35ddf54fca9b7d1d469801dd2a6a0c54478e59
johnjihwan <s20062@gsm.hs.kr>
2021-01-26 13:44:48 +0900

    [ADD]LaptopRepo

---
+++ src/main/java/com/gsm/jupjup/repo/LaptopRepo.java
@@ -0,0 +1,8 @@
+package com.gsm.jupjup.repo;
+
+import com.gsm.jupjup.model.Laptop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LaptopRepo extends JpaRepository<Laptop, String> {
+
+}
