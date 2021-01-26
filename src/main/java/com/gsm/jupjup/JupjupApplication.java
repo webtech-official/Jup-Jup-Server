24ac894dd3c0a9e8062781a85fb2d87325c56734
johnjihwan <s20062@gsm.hs.kr>
2021-01-26 17:13:37 +0900

    [ADD]JpaAuditing annotation for use baseTimeEntity

--- src/main/java/com/gsm/jupjup/JupjupApplication.java
+++ src/main/java/com/gsm/jupjup/JupjupApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @SpringBootApplication
+@EnableJpaAuditing
 public class JupjupApplication implements CommandLineRunner {
 	private final AdminRepo adminRepo;
 	private final LaptopSpecRepo laptopSpecRepo;
