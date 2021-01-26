66aaec8511d10fabdee5612805e7f14218f6368d
johnjihwan <s20062@gsm.hs.kr>
2021-01-26 17:25:12 +0900

    [DELETE]Don't use code

--- src/main/java/com/gsm/jupjup/dto/laptop/LaptopSaveRequestDto.java
+++ src/main/java/com/gsm/jupjup/dto/laptop/LaptopSaveRequestDto.java
@@ -1,12 +1,9 @@
 package com.gsm.jupjup.dto.laptop;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
