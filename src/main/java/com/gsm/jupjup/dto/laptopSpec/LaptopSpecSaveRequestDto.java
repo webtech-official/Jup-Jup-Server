06685916de6a03b7a90abb38a8ef091046a71d41
johnjihwan <s20062@gsm.hs.kr>
2021-01-26 14:28:12 +0900

    [ADD]LaptopSpecRequestDto

---
+++ src/main/java/com/gsm/jupjup/dto/laptopSpec/LaptopSpecSaveRequestDto.java
@@ -0,0 +1,12 @@
+package com.gsm.jupjup.dto.laptopSpec;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LaptopSpecSaveRequestDto {
+
+}
