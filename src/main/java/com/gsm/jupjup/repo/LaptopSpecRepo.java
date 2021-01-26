199ffacb13f6032e34821a295f3ec2ce96de6457
johnjihwan <s20062@gsm.hs.kr>
2021-01-26 13:50:51 +0900

    [UPDATE]Query statement

--- src/main/java/com/gsm/jupjup/repo/LaptopSpecRepo.java
+++ src/main/java/com/gsm/jupjup/repo/LaptopSpecRepo.java
@@ -2,7 +2,9 @@ package com.gsm.jupjup.repo;
 
 import com.gsm.jupjup.model.LaptopSpec;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LaptopSpecRepo extends JpaRepository<LaptopSpec, Long> {
-
+    @Query("select l from LaptopSpec l where l.specIdx = :idx")
+    LaptopSpec findBySpecIdx(Long idx);
 }
