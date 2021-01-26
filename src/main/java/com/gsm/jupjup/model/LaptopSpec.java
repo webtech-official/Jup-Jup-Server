c9e4b71ef91471e8076d0985c2852366ce1151ef
johnjihwan <s20062@gsm.hs.kr>
2021-01-26 13:20:03 +0900

    [ADD]Set LaptopSpec Domain

--- src/main/java/com/gsm/jupjup/model/LaptopSpec.java
+++ src/main/java/com/gsm/jupjup/model/LaptopSpec.java
@@ -1,2 +1,32 @@
-package com.gsm.jupjup.model;public class LaptopSpec {
+package com.gsm.jupjup.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class LaptopSpec {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long specIdx;
+
+    @Column
+    private String CPU;
+
+    @Column
+    private String GPU;
+
+    @Column
+    private String RAM;
+
+    @Column
+    private String SSD;
+
+    @Column
+    private String HDD;
 }
