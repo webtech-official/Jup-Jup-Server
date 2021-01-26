28467a74296462cf17a00547edd049bc030098c0
johnjihwan <s20062@gsm.hs.kr>
2021-01-26 14:23:50 +0900

    [UPDATE]Optional code

--- src/main/java/com/gsm/jupjup/repo/LaptopRepo.java
+++ src/main/java/com/gsm/jupjup/repo/LaptopRepo.java
@@ -3,6 +3,9 @@ package com.gsm.jupjup.repo;
 import com.gsm.jupjup.model.Laptop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LaptopRepo extends JpaRepository<Laptop, String> {
+import java.util.Optional;
 
+public interface LaptopRepo extends JpaRepository<Laptop, String> {
+    // 명확한 error, exception 처리.
+    Optional<Laptop> findByLaptopSerialNumber(String laptopSerialNumber);
 }
