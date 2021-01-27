c21cf2c66244083cc9b1b86270468849fa345723
siwony_ <siwon103305@gmail.com>
2021-01-27 19:43:15 +0900

    [UPDATE]EquipmentResDto 기자재 모두 조회 로직을 위한 field 밑 method 선언

--- src/main/java/com/gsm/jupjup/dto/equipment/EquipmentResDto.java
+++ src/main/java/com/gsm/jupjup/dto/equipment/EquipmentResDto.java
@@ -1,20 +1,30 @@
 package com.gsm.jupjup.dto.equipment;
 
 import com.gsm.jupjup.model.Equipment;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class EquipmentResDto {
     private String name;
-    private String img_equipment;
+    private byte[] img_equipment;
     private String content;
     private int count;
 
+    @Builder
+    public EquipmentResDto(String name, String content, byte[] img_equipment, int count){
+        this.name = name;
+        this.content = content;
+        this.img_equipment = img_equipment;
+        this.count = count;
+    }
+
     public EquipmentResDto(Equipment equipmentDomain){
         this.name = equipmentDomain.getName();
-        this.img_equipment = equipmentDomain.getImg_equipment();
         this.content = equipmentDomain.getContent();
         this.count = equipmentDomain.getCount();
     }
