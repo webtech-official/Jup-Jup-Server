61eeed1752c555f24760c566fb41c47f989950e0
siwony_ <siwon103305@gmail.com>
2021-01-27 09:41:13 +0900

    [UPDATE]equipmentUploadDto DB에 byte배열 변수를 없애고 file path를 받기 위한 String type img_equipment_location 추가

--- src/main/java/com/gsm/jupjup/dto/equipment/EquipmentUploadDto.java
+++ src/main/java/com/gsm/jupjup/dto/equipment/EquipmentUploadDto.java
@@ -11,11 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class EquipmentUploadDto {
     private MultipartFile img_equipment;
-    private byte[] img_equipmentToByte;
+    @JsonIgnore
+    private String img_equipment_location;
 
     private String name;
     private String content;
@@ -24,7 +24,6 @@ public class EquipmentUploadDto {
     @Builder
     public EquipmentUploadDto(MultipartFile img_equipment, String name, String content, int count) throws IOException {
         this.img_equipment = img_equipment;
-        this.img_equipmentToByte = img_equipment.getBytes();
         this.name = name;
         this.content = content;
         this.count = count;
@@ -33,7 +32,7 @@ public class EquipmentUploadDto {
     public Equipment toEntity(){
         return Equipment.builder()
                 .name(this.name)
-                .img_equipment(this.img_equipmentToByte)
+                .img_equipment(this.img_equipment_location)
                 .content(this.content)
                 .count(this.count)
                 .build();
