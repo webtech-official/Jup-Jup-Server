1c1fe02676381d279fc6300c4646d2ca151afc67
siwony_ <siwon103305@gmail.com>
2021-01-27 09:43:37 +0900

    [ADD]imgChk method at equipmentService

--- src/main/java/com/gsm/jupjup/service/equipment/EquipmentService.java
+++ src/main/java/com/gsm/jupjup/service/equipment/EquipmentService.java
@@ -9,7 +9,11 @@ import com.gsm.jupjup.repo.EquipmentRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -18,10 +22,7 @@ public class EquipmentService {
 
     private final EquipmentRepo equipmentRepo;
 
-    /**
-     * Equipment CRUD 구현
-     * U => Dirty Checking
-     */
+
     public void save(EquipmentUploadDto equipmentUploadDto) {
         if(equipmentUploadDto.getImg_equipment().isEmpty()) throw new ImageNotFoundException();
 
@@ -47,11 +48,32 @@ public class EquipmentService {
         return new EquipmentResDto(equipment);
     }
 
+
+    /******일반 Method 들******/
+
     //Equipment를 name으로 찾고 Entity만드는 매서드
     public Equipment equipmentFindBy(String name) throws Exception {
         return equipmentRepo.findByName(name).orElseThrow(EquipmentNotFoundException::new);
     }
 
-    //기자재 중복 처리 메소드
+    //img 파일 저장
+    public void SaveImgFile(MultipartFile img){
+        Date date = new Date();
+        StringBuilder nameOfImg = new StringBuilder();
+
+    }
+
+    /** img 예외처리 method
+     * img 를 예외체크를 해서 아무 예외도 나오지 않는다면 ture 를 반환한다.
+     * @param img
+     * @return ture(img가 아무 예외도 않나오면 반환)
+     */
+    public boolean imgChk(MultipartFile img){
+        if(img.isEmpty())
+            throw new ImageNotFoundException();
+        else
+            return true;
+
+    }
 
 }
