fbc45d8b413aafdb173a32d55e113830cb3a4be1
김상현 <llmm030506@naver.com>
2021-01-28 17:54:17 +0900

    [UPDATE]
    CORS 추가

--- src/main/java/com/gsm/jupjup/controller/v1/AdminController.java
+++ src/main/java/com/gsm/jupjup/controller/v1/AdminController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class AdminController {
     private final EquipmentService equipmentService;
     private final EquipmentAllowService equipmentAllowService;
