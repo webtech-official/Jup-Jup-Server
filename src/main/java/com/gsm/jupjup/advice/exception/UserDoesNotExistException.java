7cdcbce2a55006e5d7e2492dc99a57e6e649313c
johnjihwan <s20062@gsm.hs.kr>
2021-01-28 09:40:52 +0900

    [UPDATE]UserDoesNotExist Exception setting

--- src/main/java/com/gsm/jupjup/advice/exception/UserDoesNotExistException.java
+++ src/main/java/com/gsm/jupjup/advice/exception/UserDoesNotExistException.java
@@ -1,2 +1,14 @@
-package com.gsm.jupjup.advice.exception;public class UserDoesNotExistException {
+package com.gsm.jupjup.advice.exception;
+public class UserDoesNotExistException  extends RuntimeException{
+    public UserDoesNotExistException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public UserDoesNotExistException(String msg) {
+        super(msg);
+    }
+
+    public UserDoesNotExistException() {
+        super();
+    }
 }
--- src/main/java/com/gsm/jupjup/config/security/UserDoesNotExistHandler.java
+++ src/main/java/com/gsm/jupjup/config/security/UserDoesNotExistHandler.java
@@ -1,2 +1,18 @@
-package com.gsm.jupjup.config.security;public class UserDoesNotExistHandler {
+package com.gsm.jupjup.config.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class UserDoesNotExistHandler {
+    public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException,
+            ServletException {
+        response.sendRedirect("/exception/userdoesnotexistexception");
+    }
 }
