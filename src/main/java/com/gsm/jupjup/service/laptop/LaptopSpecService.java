d6f8f7bced4987ac01134c7687dcd4b84861c008
siwony_ <siwon103305@gmail.com>
2021-02-09 14:33:15 +0900

    [UPDATE]LaptopSpecService crud interface finish

--- src/main/java/com/gsm/jupjup/service/laptop/LaptopSpecService.java
+++ src/main/java/com/gsm/jupjup/service/laptop/LaptopSpecService.java
@@ -1,10 +1,21 @@
 package com.gsm.jupjup.service.laptop;
 
 import com.gsm.jupjup.dto.laptopSpec.LaptopSpecSaveRequestDto;
+import com.gsm.jupjup.model.LaptopSpec;
+
+import java.util.List;
 
 public interface LaptopSpecService {
 
     //Spec 저장
     Long save(LaptopSpecSaveRequestDto laptopSpecSaveRequestDto);
+
+    List<LaptopSpec> findAll();
+
+    LaptopSpec findBySpecIdx(Long SpecIdx);
+
+    void updateBySpecIdx(Long SpecIdx);
+
+    void deleteBySpecIdx(Long SpecIdx);
 
 }
