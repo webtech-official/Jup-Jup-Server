eb41108d71021c76e0df8a0bb3177173788fed9f
siwony_ <siwon103305@gmail.com>
2021-02-09 15:52:01 +0900

    [UPDATE]crud logic finish at LaptopSpecServicelmp

--- src/main/java/com/gsm/jupjup/service/laptop/LaptopSpecServiceImpl.java
+++ src/main/java/com/gsm/jupjup/service/laptop/LaptopSpecServiceImpl.java
@@ -1,10 +1,13 @@
 package com.gsm.jupjup.service.laptop;
 
-import com.gsm.jupjup.dto.laptopSpec.LaptopSpecSaveRequestDto;
+import com.gsm.jupjup.dto.laptopSpec.LaptopSpecDto;
+import com.gsm.jupjup.model.LaptopSpec;
 import com.gsm.jupjup.repo.LaptopSpecRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class LaptopSpecServiceImpl implements LaptopSpecService {
@@ -14,7 +17,34 @@ public class LaptopSpecServiceImpl implements LaptopSpecService {
     private LaptopSpecRepo laptopSpecRepo;
 
     @Override
-    public Long save(LaptopSpecSaveRequestDto laptopSpecSaveRequestDto){
-        return laptopSpecRepo.save(laptopSpecSaveRequestDto.toEntity()).getSpecIdx();
+    public Long save(LaptopSpecDto laptopSpecDto){
+        return laptopSpecRepo.save(laptopSpecDto.toEntity()).getSpecIdx();
     }
+
+    @Override
+    public List<LaptopSpec> findAll() {
+        return laptopSpecRepo.findAll();
+    }
+
+    @Override
+    public LaptopSpec findBySpecIdx(Long SpecIdx) {
+        return laptopSpecRepo.findBySpecIdx(SpecIdx);
+    }
+
+    @Override
+    @Transactional
+    public void updateBySpecIdx(Long specIdx, LaptopSpecDto laptopSpecDto) {
+        LaptopSpec laptopSpec = findBySpecIdx(specIdx);
+        laptopSpec.update(laptopSpecDto);
+    }
+
+    @Override
+    public void deleteBySpecIdx(Long SpecIdx) {
+        laptopSpecRepo.deleteById(SpecIdx);
+    }
+
+//    public void SpecIsBlank(LaptopSpecDto laptopSpecDto){
+//        if
+//    }
+
 }
