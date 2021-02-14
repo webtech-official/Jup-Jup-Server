0ede59dd8bcf8e25991efe7d330cb3c112d4e769
김상현 <llmm030506@naver.com>
2021-02-14 16:47:12 +0900

    [UPDATE] image upload

---
+++ src/main/java/com/gsm/jupjup/service/Resolver.java
@@ -0,0 +1,18 @@
+package com.gsm.jupjup.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class Resolver {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(2000000000);
+        return multipartResolver;
+    }
+
+}
