a08b820891793987c4bdc62cd2485d5e32037e1e
siwony_ <siwon103305@gmail.com>
2021-02-15 17:58:32 +0900

    [UPDATE]equipment img s3 save finish

--- src/main/java/com/gsm/jupjup/util/S3Uploader.java
+++ src/main/java/com/gsm/jupjup/util/S3Uploader.java
@@ -3,8 +3,11 @@ package com.gsm.jupjup.util;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.gsm.jupjup.advice.exception.FileExtensionNotMatchImageException;
+import com.gsm.jupjup.advice.exception.ImageNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,6 +15,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -25,6 +31,7 @@ public class S3Uploader {
     private String bucket;
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        imgChk(multipartFile); //이미지 체크
         File uploadFile = convert(multipartFile)
                 .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
 
@@ -52,7 +59,8 @@ public class S3Uploader {
     }
 
     private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(file.getOriginalFilename());
+        String fileExtension = file.getContentType().split("/")[1]; //파일확장자
+        File convertFile = new File(imgNameMake(file, fileExtension)); // 파일이름을 원래 파일이름 + 날짜 형식으로 저장
         if(convertFile.createNewFile()) {
             try (FileOutputStream fos = new FileOutputStream(convertFile)) {
                 fos.write(file.getBytes());
@@ -62,4 +70,36 @@ public class S3Uploader {
 
         return Optional.empty();
     }
+
+    /** img 예외처리 method
+     * img 를 예외체크를 해서 아무 예외도 나오지 않는다면 ture 를 반환한다.
+     * @param img
+     * @throws ImageNotFoundException, FileExtensionNotMatchImageException
+     */
+    private void imgChk(MultipartFile img){
+        if(img.isEmpty())
+            throw new ImageNotFoundException();
+        else if(!img.getContentType().split("/")[0].equals("image"))  //type 이 image 가 아니면
+            throw new FileExtensionNotMatchImageException();
+    }
+
+    /**
+     * 기자재 img 이름을 만들어주는 method
+     * 현재 시간 + 사진이름 으로 만들어 img 이름을 반환하는 매서드
+     *         이미지 이름, 이미지 확장자
+     * @param img, imageExtension
+     * @param imageExtension
+     * @return nameOfImg
+     */
+    public String imgNameMake(MultipartFile img, String imageExtension){
+        StringBuilder nameOfImg = new StringBuilder();
+
+        String originalName = img.getOriginalFilename().replace("." + img.getContentType().split("/")[1], "");
+        nameOfImg.append(originalName);
+        nameOfImg.append(new Date().getTime());
+        nameOfImg.append("." + imageExtension);
+
+        return nameOfImg.toString();
+    }
 }
+
