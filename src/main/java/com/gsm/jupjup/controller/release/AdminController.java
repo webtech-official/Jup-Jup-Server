33d74b9feb0792d8a88a38428abd6de2b4f92c4c
김상현 <llmm030506@naver.com>
2021-02-16 05:52:07 +0900

    commit

--- src/main/java/com/gsm/jupjup/controller/release/AdminController.java
+++ src/main/java/com/gsm/jupjup/controller/release/AdminController.java
@@ -1,9 +1,8 @@
-package com.gsm.jupjup.controller.release;
+package com.gsm.jupjup.controller.v1;
 
 import com.gsm.jupjup.config.handler.NotFoundImageHandler;
 import com.gsm.jupjup.dto.equipment.EquipmentResDto;
 import com.gsm.jupjup.dto.equipment.EquipmentUploadDto;
-import com.gsm.jupjup.model.Equipment;
 import com.gsm.jupjup.model.response.*;
 import com.gsm.jupjup.service.admin.AdminService;
 import com.gsm.jupjup.service.equipment.EquipmentAllowService;
@@ -20,7 +19,6 @@ import java.util.List;
 @Api(tags = {"2. 관리자"})
 @RestController
 @RequestMapping("/v1")
-@CrossOrigin(origins = "http://localhost:3000") //해당 origin 승인하기
 @RequiredArgsConstructor
 public class AdminController {
     private final EquipmentService equipmentService;
@@ -39,7 +37,7 @@ public class AdminController {
     }
 
     @ApiOperation(value = "기자재 등록", notes = "기자재를 등록한다.")
-    @PostMapping(path = "/admin/equipment", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(path = "/equipment", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
@@ -70,7 +68,7 @@ public class AdminController {
     }
 
     @ApiOperation(value = "기자재 수량 변경", notes = "기자재 수량을 변경한다.")
-    @PutMapping("/admin/equipment/{name}")
+    @PutMapping("/equipment/{name}")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
@@ -81,12 +79,12 @@ public class AdminController {
     }
 
     @ApiOperation(value = "기자재 전체 수정", notes = "기자재를 수정한다.")
-    @PutMapping("/admin/equipmentAll/{oldName}")
+    @PutMapping("/equipmentAll/{oldName}")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
     public CommonResult update( @ApiParam(value = "수정할 기자재 이름", required = true) @PathVariable String oldName,
-                                @ApiParam(value = "기자재 이미지", required = false) @RequestParam(value = "img_equipment",required = false) MultipartFile img_equipment,
+                                @ApiParam(value = "기자재 이미지", required = true) @RequestParam("img_equipment") MultipartFile img_equipment,
                                 @ApiParam(value = "기자재 이름", required = true) @RequestParam String newName,
                                 @ApiParam(value = "기자재 유형", required = true) @RequestParam String content,
                                 @ApiParam(value = "기자재 개수", required = true) @RequestParam int count) throws Exception, NotFoundImageHandler {
@@ -97,26 +95,26 @@ public class AdminController {
                         .content(content)
                         .count(count)
                         .img_equipment(img_equipment)
-                .build()
+                        .build()
         );
         return responseService.getSuccessResult();
     }
 
-    @ApiOperation(value = "기자재 삭제", notes = "기자재를 삭제한다.")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
-    })
-    @DeleteMapping("/admin/equipmnet/delete/{equipmentidx}")
-    public CommonResult deleteByIdx(@ApiParam(value = "기자재 Idx", required = true) @PathVariable Long equipmentidx) throws Exception {
-        equipmentService.deleteByEquipmentIdx(equipmentidx);
-        return responseService.getSuccessResult();
-    };
+//    @ApiOperation(value = "기자재 삭제", notes = "기자재를 삭제한다.")
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+//    })
+//    @DeleteMapping("/equipmnet/delete")
+//    public CommonResult deleteByIdx(@ApiParam(value = "기자재 이름", required = true) @RequestParam String name) throws Exception {
+//        equipmentService.deleteByName(name);
+//        return responseService.getSuccessResult();
+//    };
 
     @ApiOperation(value = "신청 전체 조회", notes = "신청을 조회한다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
-    @GetMapping("/admin/applyview")
+    @GetMapping("/applyview")
     public ListResult<Object> findAll(){
         List<Object> equipmentAllowListResult = adminService.findAll();
         return responseService.getListResult(equipmentAllowListResult);
@@ -127,7 +125,7 @@ public class AdminController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
-    @PutMapping("/admin/approved/{eqa_Idx}")
+    @PutMapping("/approved/{eqa_Idx}")
     public CommonResult ApprovedAllow(@ApiParam(value = "신청 Idx", required = true) @PathVariable Long eqa_Idx){
         equipmentAllowService.SuccessAllow(eqa_Idx);
         return responseService.getSuccessResult();
@@ -138,7 +136,7 @@ public class AdminController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
-    @PutMapping("/admin/reject/{eqa_Idx}")
+    @PutMapping("/reject/{eqa_Idx}")
     public CommonResult RejectAllow(@ApiParam(value = "신청 Idx", required = true) @PathVariable Long eqa_Idx){
         equipmentAllowService.FailureAllow(eqa_Idx);
         return responseService.getSuccessResult();
@@ -149,19 +147,9 @@ public class AdminController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
-    @PutMapping("/admin/return/{eqa_Idx}")
+    @PutMapping("/return/{eqa_Idx}")
     public CommonResult ReturnAllow(@ApiParam(value = "신청 Idx", required = true) @PathVariable Long eqa_Idx){
         equipmentAllowService.ReturnAllow(eqa_Idx);
         return responseService.getSuccessResult();
-    }
-
-    //기자재 키워드(이름) 검색
-    @ApiOperation(value = "키워드 검색", notes = "유저가 키원드를 사용하여 기자재를 조회한다.")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
-    })
-    @PostMapping("/equipment/{keyword}")
-    public ListResult<Equipment> findByKeyWord(@ApiParam(value = "검색 KeyWord", required = true) @PathVariable String keyword) throws Exception {
-        return responseService.getListResult(equipmentService.findByKeyword(keyword));
     }
 }
