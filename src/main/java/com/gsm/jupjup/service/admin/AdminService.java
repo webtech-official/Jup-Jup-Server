c5132756f28f22508d454179c8f3f8f9bb37f95d
김상현 <llmm030506@naver.com>
2021-03-08 11:10:29 +0900

    [ADD] equipmentAllowEnum Search

--- src/main/java/com/gsm/jupjup/service/admin/AdminService.java
+++ src/main/java/com/gsm/jupjup/service/admin/AdminService.java
@@ -1,8 +1,15 @@
 package com.gsm.jupjup.service.admin;
 
+import com.gsm.jupjup.model.EquipmentAllow;
+import com.gsm.jupjup.model.response.EquipmentAllowEnum;
+
 import java.util.List;
 
 public interface AdminService {
     //신청 모두 조회
-    List<Object> findAll();
+    List<EquipmentAllow> findAll();
+
+    //EquipmentAllowEnum따라 조회
+    List<EquipmentAllow> findByEquipmentAllowEnum(EquipmentAllowEnum equipmentAllowEnum);
+
 }
--- src/main/java/com/gsm/jupjup/service/admin/AdminServiceImpl.java
+++ src/main/java/com/gsm/jupjup/service/admin/AdminServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gsm.jupjup.service.admin;
 
+import com.gsm.jupjup.model.EquipmentAllow;
+import com.gsm.jupjup.model.response.EquipmentAllowEnum;
 import com.gsm.jupjup.repo.EquipmentAllowRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,10 +18,22 @@ public class AdminServiceImpl implements AdminService{
 
     /**
      * 기자재 신청 모두 찾기 => 관리자
+     * @return equipmentAllow
      */
     @Override
-    public List<Object> findAll(){
-        List<Object> equipmentAllow = equipmentAllowRepo.findAllBy();
+    public List<EquipmentAllow> findAll(){
+        List<EquipmentAllow> equipmentAllow = equipmentAllowRepo.findAllBy();
+        return equipmentAllow;
+    }
+
+    /**
+     * 기자재 거절 조회
+     * @param equipmentAllowEnum 기자재의 상태 ROLE_Reject
+     * @return equipmentAllow(ROLE_Reject)
+     */
+    @Override
+    public List<EquipmentAllow> findByEquipmentAllowEnum(EquipmentAllowEnum equipmentAllowEnum) {
+        List<EquipmentAllow> equipmentAllow = equipmentAllowRepo.findByEquipmentEnum(equipmentAllowEnum);
         return equipmentAllow;
     }
 }
