7e749033c57b928ff8560872f7cd0b3141073b66
김상현 <llmm030506@naver.com>
2021-03-10 22:36:32 +0900

    admin info

--- src/main/java/com/gsm/jupjup/service/admin/AdminServiceImpl.java
+++ src/main/java/com/gsm/jupjup/service/admin/AdminServiceImpl.java
@@ -1,7 +1,11 @@
 package com.gsm.jupjup.service.admin;
 
+import com.gsm.jupjup.model.Admin;
+import com.gsm.jupjup.repo.AdminRepo;
 import com.gsm.jupjup.repo.EquipmentAllowRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,10 +15,23 @@ import java.util.List;
 public class AdminServiceImpl implements AdminService{
 
     private final EquipmentAllowRepo equipmentAllowRepo;
+    private final AdminRepo adminRepo;
 
     @Override
     public List<Object> findAll(){
         List<Object> equipmentAllow = equipmentAllowRepo.findAllBy();
         return equipmentAllow;
+    }
+
+    @Override
+    public Admin UserInfo() {
+        return currentUser();
+    }
+
+    //현재 사용자의 ID를 Return
+    public static Admin currentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Admin user = (Admin) authentication.getPrincipal();
+        return user;
     }
 }
