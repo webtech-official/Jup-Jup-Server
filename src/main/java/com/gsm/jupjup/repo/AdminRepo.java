148ff48e16208951ace1d26b364722b5c0a9f1f6
김상현 <llmm030506@naver.com>
2021-03-14 20:35:31 +0900

    [ADD] reopsitory

--- src/main/java/com/gsm/jupjup/repo/AdminRepo.java
+++ src/main/java/com/gsm/jupjup/repo/AdminRepo.java
@@ -2,9 +2,11 @@ package com.gsm.jupjup.repo;
 
 import com.gsm.jupjup.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AdminRepo extends JpaRepository<Admin, Long> {
     Optional<Admin> findByEmail(String email);
     Admin findAllByEmail(String email);
