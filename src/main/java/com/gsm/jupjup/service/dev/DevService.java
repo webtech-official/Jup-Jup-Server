2ab844b0b9bb7a1c58845155847dd9c466db3491
김상현 <llmm030506@naver.com>
2021-03-15 11:30:21 +0900

    [UPDATE] SOLID DIP

--- src/main/java/com/gsm/jupjup/service/dev/DevService.java
+++ src/main/java/com/gsm/jupjup/service/dev/DevService.java
@@ -1,37 +1,22 @@
 package com.gsm.jupjup.service.dev;
 
 import com.gsm.jupjup.model.Admin;
-import com.gsm.jupjup.repo.AdminRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
 import java.util.List;
 
-@RequiredArgsConstructor
-@Service
-public class DevService {
-
-    private final AdminRepo adminRepo;
+public interface DevService {
 
     /**
      * 어드민으로 변경하기
      */
-    @Transactional
-    public void changeAdmin(Long admin_Idx) {
-        Admin admin = adminRepo.findById(admin_Idx).orElseThrow(null);
-        //어드민 바꾸기
-        admin.Change_Admin_Role();
-    }
+    void changeAdmin(Long admin_Idx);
 
     /**
      * 유저 검색하기
      */
-    public List<Admin> findAllAdmin() {
-        List<Admin> admin = adminRepo.findAll();
-        return admin;
-    }
+    List<Admin> findAllAdmin();
 
 
 }
