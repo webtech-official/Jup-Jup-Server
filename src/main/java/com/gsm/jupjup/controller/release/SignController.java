725a340747d99d313d337550013edf210e61aac9
김상현 <llmm030506@naver.com>
2021-06-07 10:21:03 +0900

    commit auth

--- src/main/java/com/gsm/jupjup/controller/release/SignController.java
+++ src/main/java/com/gsm/jupjup/controller/release/SignController.java
@@ -3,6 +3,7 @@ package com.gsm.jupjup.controller.release;
 import com.gsm.jupjup.advice.exception.CDuplicateEmailException;
 import com.gsm.jupjup.advice.exception.CEmailSigninFailedException;
 import com.gsm.jupjup.advice.exception.EmailNotVerifiedException;
+import com.gsm.jupjup.config.security.CustomUserDetailService;
 import com.gsm.jupjup.config.security.JwtTokenProvider;
 import com.gsm.jupjup.dto.admin.SignInDto;
 import com.gsm.jupjup.dto.admin.SignUpDto;
@@ -19,17 +20,18 @@ import com.gsm.jupjup.util.RedisUtil;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 import java.util.*;
 
 @Api(tags = {"1. 회원"})
@@ -51,6 +53,7 @@ public class SignController {
     private final PasswordEncoder passwordEncoder;
     private final RedisUtil redisUtil;
     private final CookieUtil cookieUtil;
+    private final CustomUserDetailService customUserDetailService;
 
     @ApiOperation(value = "로그인", notes = "이메일 회원 로그인을 한다.")
     @PostMapping(value = "/signin")
@@ -68,12 +71,17 @@ public class SignController {
 
         String token = jwtTokenProvider.generateToken(admin);
         refreshJwt = jwtTokenProvider.generateRefreshToken(admin);
+        System.out.println("==========");
+        System.out.println("refreshJwt = " + refreshJwt);
+        System.out.println("==========");
         redisUtil.setDataExpire(refreshJwt, admin.getUsername(), jwtTokenProvider.REFRESH_TOKEN_VALIDATION_SECOND);
+        System.out.println("Redis 저장됨 ==========");
 
         Iterator<? extends GrantedAuthority> authorityIterator = admin.getAuthorities().iterator();
         String authority = authorityIterator.next().toString();
         Map<String ,String> map = new HashMap<>();
-        map.put("accessToken", token);
+        map.put("AccessToken", token);
+        map.put("RefreshToken", refreshJwt);
         map.put("authority", authority);
         map.put("name", admin.getName());
         map.put("classNum", admin.getClassNumber());
@@ -140,5 +148,24 @@ public class SignController {
     public SingleResult<Admin> UserInfo(){
         Admin admin = adminService.UserInfo();
         return responseService.getSingleResult(admin);
+    }
+
+    @ApiOperation(value = "새로운 토큰 요청하기", notes = "유저가 비밀번호를 변경한다.")
+    @ResponseBody
+    @PostMapping("/auth/refresh")
+    public void authRefresh(String refreshJwt, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        String newAccessToken = null;
+        String RefreshTokenUserEmail = jwtTokenProvider.getUserEmail(refreshJwt);
+        String RedisRefreshJwt = redisUtil.getData(RefreshTokenUserEmail);  //현재 Redis에 저장되어 있는 리프레쉬 토큰
+
+        if(RedisRefreshJwt.equals(refreshJwt)){
+            UserDetails userDetails = customUserDetailService.loadUserByUsername(RefreshTokenUserEmail);
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            Admin admin = customUserDetailService.findAdmin( RefreshTokenUserEmail);
+            newAccessToken = jwtTokenProvider.generateToken(admin);
+            httpServletResponse.addHeader("newAccessToken", newAccessToken);
+        }
     }
 }
