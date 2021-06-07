88bdbec92f6abe5fde06fc95bc487909f1743b3e
김상현 <llmm030506@naver.com>
2021-06-08 00:33:09 +0900

    fixed untracked files

---
+++ src/test/java/com/gsm/jupjup/JupjupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm.jupjup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupjupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
